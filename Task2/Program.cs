b796b6b1c199ae7d0ec0abb474940f1468018eca
Mike <big.ben@gmail.com>
2023-01-14 16:41:09 +0300

    doc

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Clear(); // Очистка консоли
+// Console.WriteLine("Hello, World!");
+// // ctrl + / 
+// int a = 1, b = 2;
+// Console.WriteLine($"{a}, {b}");
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine("=");
+
